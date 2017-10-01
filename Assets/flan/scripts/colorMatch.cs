32ce064624107d1352f2d7c6d17258ef658750d8
Unknown <mfalacci@mail.com>
2017-10-01 16:05:57 -0400

    gfg

---
+++ Assets/flan/scripts/colorMatch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorMatch : MonoBehaviour {
+
+	public GameObject windowParent; 
+	bool colorChanged;
+
+	// Use this for initialization
+	void Start () {
+		colorChanged = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		Color newColor = windowParent.GetComponent<Renderer>().material.color; 
+
+		gameObject.GetComponent<Renderer> ().material.color = newColor;
+
+		colorChanged = true;
+	}
+}
+
+
