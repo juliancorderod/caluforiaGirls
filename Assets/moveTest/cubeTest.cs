c3b2a295a6c7c6273046e64a61385a0a2e13ea75
Unknown <jc6984@nyu.edu>
2017-10-01 13:25:19 -0400

    progress

--- Assets/moveTest/cubeTest.cs
+++ Assets/moveTest/cubeTest.cs
@@ -16,6 +16,9 @@ public class cubeTest : MonoBehaviour {
     public bool canMove = false;
     Rigidbody r;
 
+    float tim = 0;
+    bool godStart = false;
+
 	// Use this for initialization
 	void Start () {
         r = GetComponent<Rigidbody>();
@@ -25,35 +28,48 @@ public class cubeTest : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (InputManager.Instance.ThereIsInput())
+        if (transform.position.z < 990)
         {
-           posNew = InputManager.Instance.GetTouchPosition(0, Space.Self);
-           // posNew = InputManager.Instance.GetAveragePosition(Space.Self);
+
+            if (InputManager.Instance.ThereIsInput())
+            {
+                posNew = InputManager.Instance.GetTouchPosition(0, Space.Self);
+                // posNew = InputManager.Instance.GetAveragePosition(Space.Self);
+            }
+
+
+            //this is movementstuff
+            if (posOld.z < posNew.z)
+                canMove = true;
+            else
+                canMove = false;
+
+            if (canMove)
+                //	transform.position += new Vector3(0, 0, (posOld.z - posNew.z) * Time.deltaTime * 10);
+                r.velocity += new Vector3(0, 0, (posNew.z - posOld.z) * Time.deltaTime * 75);
+            else if (r.velocity.z > 0)
+                r.velocity -= new Vector3(0, 0, Time.deltaTime * 20f);
+
+            if (r.velocity.z < 0)
+                r.velocity = Vector3.zero;
+
+
+
+
+
+
+
+
+            posOld = posNew;
         }
-
-
-        //this is movementstuff
-        if (posOld.z < posNew.z)
-            canMove = true;
-        else
-            canMove = false;
-        
-        if (canMove)
-            //	transform.position += new Vector3(0, 0, (posOld.z - posNew.z) * Time.deltaTime * 10);
-            r.velocity += new Vector3(0, 0, (posNew.z - posOld.z) * Time.deltaTime * 100);
-        else if (r.velocity.z > 0)
-            r.velocity -= new Vector3(0, 0, Time.deltaTime * 20f);
-
-        if (r.velocity.z < 0)
+        else{
             r.velocity = Vector3.zero;
-
-
-
-
-
-
-
-
-        posOld = posNew;
+            tim += Time.deltaTime;
+            if (tim > 5)
+            {
+                godStart = true;
+                Debug.Log(godStart);
+            }
+        }
 	}
 }
