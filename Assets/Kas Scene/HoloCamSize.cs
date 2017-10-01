bf57f48bce87b041a4dbcf7bd4966c940ddd4ab7
Unknown <ksg304@nyu.edu>
2017-10-01 15:55:27 -0400

    all my stuff is in Kas Scene

---
+++ Assets/Kas Scene/HoloCamSize.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloPlaySDK;
+using UnityEngine.SceneManagement;
+
+public class HoloCamSize : MonoBehaviour {
+
+    private Transform player_transform;
+    HoloPlay holoCam;
+    public float endPosY;
+    public float endRotX;
+    public float endSize;
+    Vector3 playerEndPos;
+    float currentDistance;
+    float lastDistance;
+
+	void Start () {
+        player_transform = GameObject.FindGameObjectWithTag("Player").transform;
+        holoCam = GetComponent<HoloPlay>();
+        playerEndPos = new Vector3 (0, 0, 99);
+	}
+	
+	void Update () {
+        float currentDist = Vector3.Distance(new Vector3(0,0, player_transform.position.z), playerEndPos);
+        if(currentDist > 1 && currentDist < lastDistance)
+        {
+            if(holoCam.transform.position.y < endPosY)
+            {
+                holoCam.transform.localPosition += new Vector3 (0,3 * Time.deltaTime,0);
+            }
+            if (holoCam.transform.rotation.x > endRotX)
+            {
+                holoCam.transform.localEulerAngles -= new Vector3(3 * Time.deltaTime, 0, 0);
+            }
+            if(holoCam.size < endSize)
+            {
+                holoCam.size += 7.5f * Time.deltaTime;
+            }
+        }
+        if(currentDist < 1)
+        {
+            StartCoroutine(LoadOverWorld());
+        }
+
+
+        lastDistance = currentDist;
+	}
+    IEnumerator LoadOverWorld()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("overWorldFinal");
+
+    }
+}
---
+++ Assets/moveTest/KasPlayer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using HoloPlaySDK;
+
+
+public class KasPlayer : MonoBehaviour {
+
+   // public Transform fingerPos;
+    //bool initTouch = false;
+    public Vector3 posOld, posNew;
+
+    public bool turnNow;
+    public float speed;
+    public float slowDown;
+
+    public Animator catHead;
+
+    public bool canMove = false;
+    Rigidbody r;
+
+	// Use this for initialization
+	void Start () {
+        r = GetComponent<Rigidbody>();
+
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        if (InputManager.Instance.ThereIsInput())
+        {
+            posNew = InputManager.Instance.GetTouchPosition(0, Space.World);
+            // posNew = InputManager.Instance.GetAveragePosition(Space.Self);
+        }
+
+
+        //this is movementstuff
+        if (posOld.z < posNew.z)
+            canMove = true;
+        else
+            canMove = false;
+
+        if (canMove) { 
+        //	transform.position += new Vector3(0, 0, (posOld.z - posNew.z) * Time.deltaTime * 10);
+            r.velocity += new Vector3(0, 0, (posNew.z - posOld.z) * Time.deltaTime * speed);
+
+            catHead.speed = r.velocity.z;
+        }
+            
+        else if (r.velocity.z > 0)
+        {
+
+            r.velocity -= new Vector3(0, 0, Time.deltaTime * slowDown);
+            catHead.speed = 0;
+        }
+
+        //if (r.velocity.z < 0)
+        //    r.velocity = Vector3.zero;
+
+
+
+
+
+
+
+
+        posOld = posNew;
+	}
+}
